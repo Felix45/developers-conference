493868f609beddbab1642218de5c012bd818ab01
felix45 <fatonoh@gmail.com>
2023-07-18 13:36:34 +0300

    Add favorite button toggling functionality

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Speaker from "./Speaker";
 import { v4 as uuidv4 } from 'uuid';
+import { data } from '../../SpeakerData';
 
-const SpeakersList = ({data, disp}) => {
+const SpeakersList = ({disp}) => {
+
+  const [speakerData, setSpeakerData] = useState(data);
+
+  const onFavoriteToggle = (id) => {
+    const speakerDataNew = speakerData.map((speaker) => {
+      return speaker.id === id ? {...speaker, favorite: !speaker.favorite} : speaker;
+    });
+    setSpeakerData(speakerDataNew);
+  }
+
   return (
     <div className="container speakers-list">
      <div className='row'>
       {
-        data.map((speaker) => <Speaker key={uuidv4()} speaker={speaker} disp={disp} />)
+        speakerData.map((speaker) => <Speaker key={uuidv4()} speaker={speaker} disp={disp} onFavoriteToggle={() => onFavoriteToggle(speaker.id)}  />)
       }
     </div>
   </div>
