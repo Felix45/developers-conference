105d05e25e6a2fc3faaa76ebc1f9887216ff6905
felix45 <fatonoh@gmail.com>
2023-07-18 12:38:41 +0300

    Refactor speakers component

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Speaker from "./Speaker";
-import { v4 as uuidv4 } from 'uuid';
+import SpeakersList from "./SpeakersList";
+import SpeakersToolbar from "./SpeakersToolbar";
 
-const Speakers = ({data}) => {
+const Speakers = ({data, disp, setDisp, theme, setTheme}) => {
   return (
-    <div className="container speakers-list">
-      <div className='row'>
-        {
-          data.map((speaker) => <Speaker key={uuidv4()} speaker={speaker} />)
-        }
-      </div>
-    </div>
+    <>
+      <SpeakersToolbar
+        disp={disp}
+        setDisp={setDisp}
+        theme={theme}
+        setTheme={setTheme} 
+      />
+      <SpeakersList data={data} disp={disp} />
+    </>
   );
 }
 
