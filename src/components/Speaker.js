e92ac740fc0c13fa3f8f1c065cf8ea7de29ef024
felix45 <fatonoh@gmail.com>
2023-07-21 03:39:55 +0300

    Remove key prop from speaker component

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -3,12 +3,11 @@ import Sessions from './Sessions';
 import SpeakerImage from './SpeakerImage';
 import SpeakerDemographics from './SpeakerDemographics';
 
-const Speaker = ({key, speaker, disp, onFavoriteToggle}) => {
+const Speaker = ({speaker, disp, onFavoriteToggle}) => {
     const {id, first, last, sessions} = speaker;
 
     return (
       <div
-        key = {key}
         className='col-xs-12 col-sm-12 col-md-6 col-lg-4'>
         <div className="card card-height p-4 mt-4">
           <SpeakerImage id={id} first={first} last={last} />
