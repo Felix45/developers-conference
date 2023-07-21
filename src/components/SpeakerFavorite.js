29deb16815492dadf9625a0c74dcf68b0c845f69
felix45 <fatonoh@gmail.com>
2023-07-21 03:59:21 +0300

    Animate UI when favorite button is clicked

--- src/components/SpeakerFavorite.js
+++ src/components/SpeakerFavorite.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 const SpeakerFavorite = ({favorite, onFavoriteToggle}) => {
+  const [inTransition, setInTransition] = useState(false);
+  const doneCallback = () => {
+    setInTransition(false);
+  }
 
   return (
    <div className="action padB1">
      <span
-      onClick={onFavoriteToggle}
+      onClick={() => {
+        setInTransition(true);
+        return onFavoriteToggle(doneCallback) 
+      }}
      >
         <i className={
             favorite ? "fa fa-star orange" : "fa fa-star-o orange"
            }
         /> Favourite {" "}
+        { inTransition && <span className="fas fa-circle-notch fa-spin"></span> }
      </span>
    </div>
   );
