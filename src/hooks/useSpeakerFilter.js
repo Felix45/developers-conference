d7dd17bdb1adcabc602b3d61d8a877f12110ffc7
felix45 <fatonoh@gmail.com>
2023-08-21 04:22:48 +0300

    Add useSpeakerFilter hook

--- src/hooks/useSpeakerFilter.js
+++ src/hooks/useSpeakerFilter.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 
-const useSpeakerFilter = (startingShowSessions) => {
+const useSpeakerFilter = (startingShowSessions, startingYear) => {
   const [showSessions, setShowSessions] = useState(startingShowSessions);
+  const [eventYear, setEventYear] = useState(startingYear);
+  const [searchQuery,  setSearchQuery]   = useState("");
+
+  const EVENT_YEARS = [
+    "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"
+  ];
+
 
   return {
     showSessions,
-    setShowSessions
+    setShowSessions,
+    eventYear,
+    setEventYear,
+    searchQuery,
+    setSearchQuery,
+    EVENT_YEARS
   };
 
 }
