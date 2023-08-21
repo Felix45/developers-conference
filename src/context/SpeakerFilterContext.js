d355efe8510b0184fcc6623147d21acdb74e55c9
felix45 <fatonoh@gmail.com>
2023-08-21 04:23:28 +0300

    Add SpeakersFilterContext component

--- src/context/SpeakerFilterContext.js
+++ src/context/SpeakerFilterContext.js
@@ -3,12 +3,27 @@ import useSpeakerFilter from "../hooks/useSpeakerFilter";
 
 export const SpeakerFilterContext = createContext();
 
-const SpeakerFilterProvider = ({children, startingShowSessions}) => {
+const SpeakerFilterProvider = ({children, startingShowSessions = false, startingEventYear = "2019"}) => {
    
-    const {showSessions, setShowSessions} = useSpeakerFilter(startingShowSessions);
+    const {
+        showSessions,
+        setShowSessions,
+        eventYear,
+        setEventYear,
+        searchQuery,
+        setSearchQuery,
+        EVENT_YEARS
+    } = useSpeakerFilter(startingShowSessions, startingEventYear);
 
     return (
-      <SpeakerFilterContext.Provider value={{showSessions, setShowSessions}}>
+      <SpeakerFilterContext.Provider value={
+        {
+            showSessions, setShowSessions,
+            eventYear, setEventYear,
+            searchQuery, setSearchQuery,
+            EVENT_YEARS
+        }
+      }>
         {children}
       </SpeakerFilterContext.Provider>
     );
